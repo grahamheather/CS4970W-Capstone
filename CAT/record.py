ab8bbc60f87a14bf3b9bd41d3551dbebe9b02c4d
Rap <hmgxdd@mail.missouri.edu>
2019-01-30 11:40:36 -0600

    Debug and add unit tests

--- CAT/record.py
+++ CAT/record.py
@@ -1,5 +1,6 @@
 import collections
 import datetime
+from os import path
 
 import pyaudio
 import wave
@@ -9,6 +10,7 @@ import webrtcvad
 MILLISECONDS_PER_SECOND = 1000
 
 # audio recording settings
+VAD_LEVEL = 2 # "integer between 0 and 3. 0 is the least aggressive about filtering out non-speech, 3 is the most aggressive." -py-webrtcvad docs
 FORMAT = pyaudio.paInt16 # WebRTC VAD only accepts 16-bit audio
 NUM_BYTES = 2 # 16 bits in format = 2 bytes in format
 CHANNELS = 1 # WebRTC VAD only accepts mono audio
@@ -18,10 +20,10 @@ FRAME_SIZE = int(RATE * FRAME_MS / MILLISECONDS_PER_SECOND)
 FRAME_BYTES = FRAME_SIZE * NUM_BYTES
 
 # settings based on system timing and situation
-PERIODIC_SAMPLE_RATE = 1  # how often to check when no speech has been detected, in seconds
-MIN_SAMPLE_LENGTH = 5 # smallest sample to save, in seconds
+PERIODIC_SAMPLE_RATE = .5  # how often to check when no speech has been detected, in seconds
+MIN_SAMPLE_LENGTH = .75 # smallest sample to save, in seconds
 MAX_SAMPLE_LENGTH = 30 # largest sample to save (larger ones will be split), in seconds
-MAX_SILENCE_LENGTH = 2 # largest length of silence to include in a single sample
+MAX_SILENCE_LENGTH = .5 # largest length of silence to include in a single sample
 
 # calculated from system settings
 PERIODIC_SAMPLE_FRAMES = int(PERIODIC_SAMPLE_RATE * MILLISECONDS_PER_SECOND / FRAME_MS)
@@ -30,13 +32,13 @@ MAX_SAMPLE_FRAMES = int(MAX_SAMPLE_LENGTH * MILLISECONDS_PER_SECOND / FRAME_MS)
 MAX_SILENCE_FRAMES = int(MAX_SILENCE_LENGTH * MILLISECONDS_PER_SECOND / FRAME_MS)
 
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# ADD LOGGING
-# ADD COMMENTS
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
 def open_stream():
+	''' Opens audio recording stream
+
+		Returns: the audio stream
+		Return type: pyaudio.Stream
+	'''
+
 	audio_input = pyaudio.PyAudio()
 	stream = audio_input.open(format=FORMAT,
 							  channels=CHANNELS,
@@ -48,10 +50,23 @@ def open_stream():
 	return stream
 
 
-def save_to_file(data):
+def save_to_file(audio_buffer):
+	''' Saves an audio buffer to a file
+
+		Parameters:
+			audio_buffer
+				the audio buffer to save to the file
+				type: list of byte strings
+	'''
+
 	# CHECK IF THERE IS ENOUGH SPACE REMAINING
 	# OTHERWISE SWITCH TO JUST PROCESSING, NO RECORDING
-	wave_file = wave.open("audio{}.wav".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")), 'wb')
+
+	# join segments of audio into a single byte string
+	data = b''.join(segment for segment in audio_buffer)
+
+	# save file with unique name indicating date and time
+	wave_file = wave.open(path.join("CAT", "recordings", "audio{}.wav".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S%f"))), 'wb')
 	wave_file.setnchannels(CHANNELS)
 	wave_file.setsampwidth(NUM_BYTES)
 	wave_file.setframerate(RATE)
@@ -60,41 +75,47 @@ def save_to_file(data):
 
 
 def record():
+	''' Records and saves detected speech, discarding silence '''
+
+	# set up
 	stream = open_stream()
-	vad = webrtcvad.Vad(1)
+	vad = webrtcvad.Vad(VAD_LEVEL)
 
-	last_speech = None
-	audio_buffer = collections.deque()
-	num_frames = 0
-	current_read_frames = 0
+	last_speech = None # how many frames ago speech was detected (None indicates no recent speech)
+	audio_buffer = [] # holds detected speech
+	current_read_frames = 0 # indicates how many frames were just read
 
-	while True:
-		if not last_speech == None:
+	while not stream.is_stopped(): # record continuously
+		if not last_speech == None: # if there has been recent speech, read a small section to analyze
 			audio = stream.read(FRAME_SIZE)
 			current_read_frames = 1
-		else:
+		else: # if there has not been recent speech, only check for speech periodically
 			audio = stream.read(FRAME_SIZE * PERIODIC_SAMPLE_FRAMES)
 			current_read_frames = PERIODIC_SAMPLE_FRAMES
 
-		if vad.is_speech(audio[-FRAME_BYTES:], RATE):
+		if vad.is_speech(audio[-FRAME_BYTES:], RATE): # if speech has been detected
 			# TAKE LOCK
-			last_speech = 0
-			audio_buffer.append(audio)
-			if len(audio_buffer) > MAX_SAMPLE_FRAMES:
-				save_to_file(audio_buffer)
-				audio_buffer.clear()
-		elif not last_speech == None:
-			last_speech += 1
-			if last_speech > MAX_SILENCE_FRAMES:
-				if last_speech - len(audio_buffer) > MIN_SAMPLE_FRAMES:
-					for i in range(last_speech):
-						audio_buffer.pop()
-					data = b''.join(segment for segment in audio_buffer)
-					save_to_file(data)
-				audio_buffer.clear()
+
+			last_speech = 0 # update that speech has been detected
+			audio_buffer.append(audio) # add speech
+			if len(audio_buffer) > MAX_SAMPLE_FRAMES: # if speech buffer is getting long
+				save_to_file(audio_buffer) # save incomplete speech sequence to file
+				audio_buffer = [] # and clear buffer
+		elif not last_speech == None: # otherwise if still possibily in a sequence of speech
+			last_speech += 1 # update how long it has been since last speech
+			audio_buffer.append(audio) # add this momentary silence to the buffer (to prevent choppy audio)
+
+			if last_speech > MAX_SILENCE_FRAMES: # if the pause is long enough to indicate an end to speech
+				if len(audio_buffer) - last_speech > MIN_SAMPLE_FRAMES: # only save if the detected speech is long enough
+					audio_buffer = audio_buffer[:-last_speech] # only save speech until last detected speech (discard silence)
+					save_to_file(audio_buffer) 
+
+				# reset to no speech recently detected
+				audio_buffer = []
 				last_speech = None
+
 				# RELEASE LOCK
-				
+
 
 if __name__ == '__main__':
 	record()
--- test/test_record.py
+++ test/test_record.py
@@ -2,36 +2,185 @@ import pytest
 
 # supporting libraries
 import pyaudio
+import wave
+from os import path
+import os
 
 # file under test
 from CAT import record
+
+
+# UTILITY FUNCTIONS
+def get_recording_dir():
+	return path.join('CAT', 'recordings')
+
+def get_test_recording_dir():
+	return path.join('test', 'test_recordings')
+
+def read_wav(filename):
+	wave_file = wave.open(filename, 'r')
+	return wave_file.readframes(wave_file.getnframes())
+
+
+# FIXTURES
+
+# fixture wrapping a wave file reader to the microphone input reader
+@pytest.fixture()
+def mock_stream(request, monkeypatch):
+	class MockStream:
+		i = 0 # track how many frames have been read
+
+		def __init__(self, filename):
+			self.wave_file = wave.open(filename, 'r')
+			self.n = self.wave_file.getnframes()
+			assert self.wave_file.getnchannels() == record.CHANNELS
+			assert self.wave_file.getsampwidth() == record.NUM_BYTES
+			assert self.wave_file.getframerate() == record.RATE
+
+		def read(self, frame_size):
+			self.i = self.i + frame_size
+			return self.wave_file.readframes(frame_size)
+			
+
+		def is_stopped(self):
+			# note that this might cut off the end of the audio file
+			return (self.i >= self.n - record.FRAME_SIZE * record.PERIODIC_SAMPLE_FRAMES)
+
+
+	def mockreturn():
+		return MockStream(request.param)
+
+	monkeypatch.setattr(record, "open_stream", mockreturn)
+
+
+# remove all recordings from previous tests before each new test
+@pytest.fixture(autouse=True)
+def cleanup():
+	for filename in os.listdir(get_recording_dir()):
+		file_path = path.join(path.join(get_recording_dir(), filename))
+		try:
+			os.remove(file_path)
+		except Exception as e:
+			print(e)
+
+
+# TESTS
 
 # test opening stream
 def test_open_stream():
 	result = record.open_stream()
 	assert type(result) == pyaudio.Stream
 	
-
 # test saving to file
+def test_save_to_file():
+	wave_file = wave.open(path.join(get_test_recording_dir(), 'hello.wav'), 'r')
+	audio_buffer = [wave_file.readframes(record.FRAME_SIZE) for i in range( int(wave_file.getnframes() / record.FRAME_SIZE + .5) ) ]
+	record.save_to_file(audio_buffer)
+	assert len(os.listdir(get_recording_dir())) == 1
+
+	saved_file = read_wav(path.join(get_recording_dir(), os.listdir(get_recording_dir())[0]))
+	wave_file.rewind()
+	assert saved_file == wave_file.readframes(wave_file.getnframes())
 
 # test saving a file if the disk is full
 
 # test that feeding only silence will not save a file
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'silence.wav')], indirect=['mock_stream'])
+def test_silence(mock_stream):
+	record.record()
+	assert os.listdir(get_recording_dir()) == []
 
 # test that speech less than the min sample length will not save a file
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'hello.wav')], indirect=['mock_stream'])
+def test_short(mock_stream):
+	record.record()
+	assert os.listdir(get_recording_dir()) == []
 
 # test that speech less than min sample length will not save a file but future speech will save a file
+# (check file length and contents)
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'hello+how.wav')], indirect=['mock_stream'])
+def test_short_long(mock_stream):
+	record.record()
+	assert len(os.listdir(get_recording_dir())) == 1
+	recording = read_wav(path.join(get_recording_dir(), os.listdir(get_recording_dir())[0]))
+	original = read_wav(path.join(get_test_recording_dir(), 'hello+how.wav'))
+	desired_speech = original[230000:335000]
+	assert desired_speech in recording
+	assert (len(recording) - len(desired_speech)) / (record.NUM_BYTES * record.RATE) < 0.5
 
 # test that speech between the min and max sample lengths saves a single file
 # (check file length and contents)
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'ground.wav')], indirect=['mock_stream'])
+def test_normal(mock_stream):
+	record.record()
+	assert len(os.listdir(get_recording_dir())) == 1
+	recording = read_wav(path.join(get_recording_dir(), os.listdir(get_recording_dir())[0]))
+	original = read_wav(path.join(get_test_recording_dir(), 'ground.wav'))
+	desired_speech = original[:230000]
+	assert desired_speech in recording
+	assert (len(recording) - len(desired_speech)) / (record.NUM_BYTES * record.RATE) < 0.5
 
 # test that speech longer than the max sample length will save multiple files
 # (check file length and contents)
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'tale.wav')], indirect=['mock_stream'])
+def test_too_long(mock_stream):
+	record.record()
+	files = sorted(os.listdir(get_recording_dir()))
+	assert len(files) == 2
+	recording1 = read_wav(path.join(get_recording_dir(), files[0]))
+	recording2 = read_wav(path.join(get_recording_dir(), files[1]))
+	recording = recording1 + recording2
+	original = read_wav(path.join(get_test_recording_dir(), 'tale.wav'))
+	desired_speech = original[2870000:7000000]
+	assert desired_speech in recording
+	assert (len(recording) - len(desired_speech)) / (record.NUM_BYTES * record.RATE) < .5
 
 # test that speech interspersed with silence saves appropriately
+# (check file length and contents)
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'star.wav')], indirect=['mock_stream'])
+def test_pauses(mock_stream):
+	record.record()
+	files = sorted(os.listdir(get_recording_dir()))
+	assert len(files) == 2 # the first section of speech is below the length threshold
+	recording2 = read_wav(path.join(get_recording_dir(), files[0]))
+	recording3 = read_wav(path.join(get_recording_dir(), files[1]))
+	original = read_wav(path.join(get_test_recording_dir(), 'star.wav'))
+	desired_speech2 = original[2040000:2160000]
+	desired_speech3 = original[2640000:2800000]
+	assert desired_speech2 in recording2
+	assert desired_speech3 in recording3
+	assert (len(recording2) - len(desired_speech2)) / (record.NUM_BYTES * record.RATE) < .5
+	assert (len(recording3) - len(desired_speech3)) / (record.NUM_BYTES * record.RATE) < .5
 
 # test distinguishing speech from background noise
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'noise.wav')], indirect=['mock_stream'])
+def test_noise(mock_stream):
+	record.record()
+	assert len(os.listdir(get_recording_dir())) == 1
+	recording = read_wav(path.join(get_recording_dir(), os.listdir(get_recording_dir())[0]))
+	original = read_wav(path.join(get_test_recording_dir(), 'noise.wav'))
+	desired_speech = original[1250000:1470000]
+	assert desired_speech in recording
+	assert (len(recording) - len(desired_speech)) / (record.NUM_BYTES * record.RATE) < .5
 
 # test separating multiple voices from silence
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'hello+como.wav')], indirect=['mock_stream'])
+def test_multivoice(mock_stream):
+	record.record()
+	assert len(os.listdir(get_recording_dir())) == 1
+	recording = read_wav(path.join(get_recording_dir(), os.listdir(get_recording_dir())[0]))
+	original = read_wav(path.join(get_test_recording_dir(), 'hello+como.wav'))
+	desired_speech = original[1640000:1790000]
+	assert desired_speech in recording
+	assert (len(recording) - len(desired_speech)) / (record.NUM_BYTES * record.RATE) < .5
 
 # test separating multiple voices from background noise
+@pytest.mark.parametrize('mock_stream', [path.join(get_test_recording_dir(), 'hello+como_noise.wav')], indirect=['mock_stream'])
+def test_multivoice(mock_stream):
+	record.record()
+	assert len(os.listdir(get_recording_dir())) == 1
+	recording = read_wav(path.join(get_recording_dir(), os.listdir(get_recording_dir())[0]))
+	original = read_wav(path.join(get_test_recording_dir(), 'hello+como_noise.wav'))
+	desired_speech = original[1645000:1785000]
+	assert desired_speech in recording
+	assert (len(recording) - len(desired_speech)) / (record.NUM_BYTES * record.RATE) < .5
